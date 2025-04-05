5c4ce238bd2a645c1a43b14a2730e31c6a5d4d82
Henry Liang <117045848+username0106@users.noreply.github.com>
2025-04-05 14:31:09 -0700

    finished fields and added method names (not yet completed methods)

--- Seasons.java
+++ Seasons.java
@@ -1,3 +1,14 @@
 public class Seasons {
-    
+    private Episode[] episodes;
+    private Character[] chars; 
+    private int seasonNum; 
+    private String synopsis; 
+
+    public Episode getTopEpisode(){
+        //TODO get the highest rated episode in the array of episode
+    }
+
+    public Episode[] getTopEpisode(int numOfEp){
+        //TODO get a number of top episodes from each "section" of the season
+    }
 }
