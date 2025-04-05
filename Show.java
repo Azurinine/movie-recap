e378f73c109551905454c8ce14e59d282f8a81fa
Achintya Rai <achintyaraism2@gmail.com>
2025-04-05 14:04:03 -0700

    Basic Class Pickup

---
+++ Show.java
@@ -0,0 +1,20 @@
+public class Show extends ShowAbs {
+    // Example property
+    private String title;
+
+    // Constructor
+    public Show(String title) {
+        // Calls API and gets info
+        // Gets Show title, 
+
+    }
+
+    // Getter for title
+    public String getTitle() {
+        return title;
+    }
+
+    // Main method for testing
+    // You can remove this if you don't need it
+    // or if you want to test in a different way.
+}
