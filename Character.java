33ced0b91e98ef1f668520eb7456066499d7a0c8
Henry Liang <117045848+username0106@users.noreply.github.com>
2025-04-05 14:14:57 -0700

    name and summary have getters and setters

--- Character.java
+++ Character.java
@@ -1,4 +1,20 @@
 public class Character {
     private String name;
     private String summary;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSummary(){
+        return summary;
+    }
+
+    public void setName(String name){
+        this.name = name;  
+    }
+    
+    public void setSummary(String summary){
+        this.summary = summary;
+    }
 }
