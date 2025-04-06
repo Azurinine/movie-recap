dd76484f97bab081940e0cc3e0d4c74562046e44
Achintya Rai <achintyaraism2@gmail.com>
2025-04-06 00:47:16 -0700

    TS WORK HELL YEAH

--- searchdemo/src/main/java/com/example/searchdemo/Seasons.java
+++ searchdemo/src/main/java/com/example/searchdemo/Seasons.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+
 public class Seasons {
     public ArrayList<Episode> episodes;
     private int seasonNum;
@@ -47,18 +49,23 @@ public class Seasons {
     }
 
     public ArrayList<Episode> getTopEpisode(int numOfEp){
-        //TODO get a number of top episodes from each "section" of the season
         ArrayList<Episode> topEpisodes = new ArrayList<>();
         Collections.sort(episodes, Comparator.comparing(Episode::getRating));
-        int s = episodes.size();
-        for(int i = 1; i <= numOfEp; i++){
-            if(episodes.get(s-i).equals("N/A")){
-                continue;
-            }
-            else{
-                topEpisodes.add(episodes.get(s-i));
+        System.out.println("Debug - Sorted episodes by rating" + episodes);
+        ArrayList<Episode> temp = episodes;
+
+        for (int i = 0; i < temp.size(); i++){
+            if(temp.get(i).getRating().equals("N/A")){
+                temp.remove(i);
+                i--;
             }
         }
+        System.out.println("Debug - Filtered episodes by rating" + temp);
+        int size = temp.size();
+        for (int i = 1; i <= numOfEp && i < size; i++){
+            topEpisodes.add(temp.get(size - i));
+        }
+        
         return topEpisodes;
     }
 }
