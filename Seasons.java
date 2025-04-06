5739106018db119be212b5c68c1af80e1fd2d919
josh <victoriajosh01@gmail.com>
2025-04-05 18:37:47 -0700

    Found episode Numbers

--- Seasons.java
+++ Seasons.java
@@ -1,5 +1,8 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 public class Seasons {
-    private final Episode[] episodes;
+    private final ArrayList<Episode> episodes;
     private final int seasonNum; 
     private final String name; 
     // private Character[] chars; 
@@ -7,12 +10,18 @@ public class Seasons {
 
     public Seasons(String seasonInfo) {
         // TODO
-
+        
         // Pareses String and creates Episode Objects and appends to the array
         /*Parse through the list given from the api key. Then get each episode, their name, and rating and create a new episodes 
          * object. 
          */
-        episodes = null;
+        String pattern = "\"Episode\":\\s*(1[0-9]|20|[0-9])gm";
+        Pattern episodePat = Pattern.compile(pattern);
+        Matcher matcher = episodePat.matcher(seasonInfo);
+        while (matcher.find()) {
+            Episode newEpisode = new Episode("poop", "yeah", "Hell yeah", matcher.group());
+            
+        }
         seasonNum = 0;
         name = null;
     }
